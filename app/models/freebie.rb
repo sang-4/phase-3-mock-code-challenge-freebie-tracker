3b30fb154f62f1b1259d2432cfdc1f4e806cb17e
Allan <kipropsallan@gmail.com>
2022-09-04 14:46:08 +0300

    Complete

--- app/models/freebie.rb
+++ app/models/freebie.rb
@@ -1,3 +1,13 @@
 class Freebie < ActiveRecord::Base
+    belongs_to :company
+    belongs_to :dev
 
+    def self.print_details
+        Freebie.all.map do |freebie_row|
+        "#{freebie_row.dev.name} owns a #{freebie_row.item_name} from #{freebie_row.company.name}"
+    end
+
+    def print_details
+        "#{self.dev.name} owns a #{self.item_name} from  #{self.company.name}"
+    end
 end
